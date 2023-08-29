909f54e748ec26bc2de3496bcbcb9b91466e0170
Yugencio <Yugoso8512@gmail.com>
2023-08-29 20:03:49 +0200

    funcionalidades del administrador en beta

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entradaTeclado=new Scanner(System.in);
+
         Short opcionMenu=0;
-        String[] nombresEspecialistas=new String[100];
-        Integer[] cedulasEspecialistas=new Integer[100];
-        String[] especialidades=new String[100];
-        Integer[] salariosEspecialistas=new Integer[100];
-        String[] fechasDeIngreso=new String[100];
+        Integer tamanoArreglo=100;
+        Integer index=0;
+        String[] nombresEspecialistas=new String[tamanoArreglo];
+        Integer[] cedulasEspecialistas=new Integer[tamanoArreglo];
+        String[] especialidades=new String[tamanoArreglo];
+        Integer[] salariosEspecialistas=new Integer[tamanoArreglo];
+        String[] fechasDeIngreso=new String[tamanoArreglo];
+        String continuar;
 
-
-        System.out.println("\u001B[34m ***SOMOS GRUPO SURA***  \u001B[0m");
+        System.out.println("\u001B[34m ***SOMOS HEALTHY SOLUTIONS SAS***  \u001B[0m");
         System.out.println("\u001B[34m --BIENVENIDO USUARIO--  \u001B[0m");
         System.out.println("¿Eres Admin, especialista o paciente?");
 
@@ -50,20 +53,60 @@ public class Main {
 
                     case 1:
 
-                            entradaTeclado.nextLine();
 
-                            System.out.println("Digite el nombre del especialista");
-                            nombresEspecialistas[0] = entradaTeclado.nextLine();
-                            for (int contador=0; contador < 100; contador=contador++)
 
-                                contador++;
+                         do {
+                             entradaTeclado.nextLine();
+
+                                   System.out.println("Digite el nombre del especialista");
+                                   nombresEspecialistas[index] = entradaTeclado.nextLine();
+
+
+                             System.out.println("Digite la cedula del especialista");
+                             cedulasEspecialistas[index] = entradaTeclado.nextInt();
+
+
+                             System.out.println("Digite la especialidad del especialista");
+                             especialidades[index] = entradaTeclado.nextLine();
+                             entradaTeclado.nextLine();
+
+                             System.out.println("Digite el salario del especialista");
+                             salariosEspecialistas[index] = entradaTeclado.nextInt();
+
+
+                             System.out.println("Digite la fecha de ingreso del especialista");
+                             fechasDeIngreso[index] = entradaTeclado.nextLine();
+
+
+
+
+                                   if (index < tamanoArreglo -1) {
+                                       System.out.println("\u001B[34m ¿Desea añadir otro especialista?  \u001B[0m");
+                                       System.out.println("1. Si (true)");
+                                       System.out.println("2. No (false)");
+                                       System.out.println("\u001B[34m -->   \u001B[0m");
+
+                                       continuar = entradaTeclado.next();
+                                       entradaTeclado.nextLine();
+
+                                   }else {
+                                       continuar="false";
+                                   }
+
+                               }while(continuar.equalsIgnoreCase("true"));
+                        index++;
                             break;
 
                     case 2:
                         System.out.println("Esta es la base de datos de todos los especialistas registrados");
-                        for (int contador=0; contador < 100; contador=contador++)
-                            contador++;
-                        System.out.println(nombresEspecialistas[99]);
+                        for (int contador = 0; contador < index; contador++) {
+
+                            System.out.println("Nombre: " + nombresEspecialistas[contador]+
+                                    ", con numero de cedula: "+cedulasEspecialistas[contador]+" y especialidad "
+                                    +especialidades[contador]+", un salario de "+"$"+salariosEspecialistas[contador]
+                            +" y una fecha de ingreso del "+fechasDeIngreso[contador]);
+                        }
+
                         break;
 
                 }
